0451c24a20352f9c33d9242543eb4b213312041c
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-01-21 00:31:22 -0500

    FIXED NULL POINTER THING

--- Test_Rig/Assets/Scripts/Management.cs
+++ Test_Rig/Assets/Scripts/Management.cs
@@ -25,21 +25,24 @@ public class Management : MonoBehaviour
     {
         using (var reader = new StreamReader("Assets/Data/data.csv"))
         {
-            int girth = 10000;
-            while (girth > 0) // (!reader.EndOfStream)
+            int numOfPoints = 500;
+            int count = 0;
+            while (numOfPoints > 0) // (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
                 string[] values = line.Split(',');
                 
-                float lat = float.Parse(values[0].Replace("\"", ""));
-                float lon = float.Parse(values[1].Replace("\"", ""));
+                float lat = float.Parse(values[0]);
+                float lon = float.Parse(values[1]);
                 Debug.Log(lon + " " + lat);
+                count++;
                 var instance = Instantiate(prefab , parent.transform);
                 instance.GetComponent<CesiumGlobeAnchor>().latitude = lat;
                 instance.GetComponent<CesiumGlobeAnchor>().longitude = lon;
                 
-                girth--;
+                numOfPoints--;
             }
+            Debug.Log(count);
         }
     }
 
