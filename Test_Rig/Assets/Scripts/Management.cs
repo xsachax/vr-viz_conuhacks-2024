8f13fc4680a2b4dd2c5fc01c57c26faecfc5ce3e
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-01-20 23:44:11 -0500

    fix marker issue

--- Test_Rig/Assets/Scripts/Management.cs
+++ Test_Rig/Assets/Scripts/Management.cs
@@ -25,18 +25,20 @@ public class Management : MonoBehaviour
     {
         using (var reader = new StreamReader("Assets/Data/data.csv"))
         {
-            while (!reader.EndOfStream)
+            int girth = 10000;
+            while (girth > 0) // (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
                 string[] values = line.Split(',');
                 
-                float lon = float.Parse(values[0].Replace("\"", ""));
-                float lat = float.Parse(values[1].Replace("\"", ""));
+                float lat = float.Parse(values[0].Replace("\"", ""));
+                float lon = float.Parse(values[1].Replace("\"", ""));
                 Debug.Log(lon + " " + lat);
-                var instance = Instantiate(prefab, parent.transform);
+                var instance = Instantiate(prefab , parent.transform);
                 instance.GetComponent<CesiumGlobeAnchor>().latitude = lat;
                 instance.GetComponent<CesiumGlobeAnchor>().longitude = lon;
-                instance.transform.position = new Vector3(0, -10000, 0);
+                
+                girth--;
             }
         }
     }
